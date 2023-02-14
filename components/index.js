e098585a4326da126f07ac18ad4f8babd28e442f
hailatGH <hailat.alx@gmail.com>
2023-02-14 08:41:35 +0300

    feb 7 morning start

---
+++ components/index.js
@@ -0,0 +1,26 @@
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import HomeHeader from "./HomeHeader";
+import { CircleButton, RectButton } from "./Button";
+import {
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  SubInfo,
+} from "./SubInfo";
+
+export {
+  FocusedStatusBar,
+  NFTCard,
+  HomeHeader,
+  CircleButton,
+  RectButton,
+  NFTTitle,
+  EthPrice,
+  ImageCmp,
+  People,
+  EndDate,
+  SubInfo,
+};
